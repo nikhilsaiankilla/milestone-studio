c5023bd665db2f9943c6a6e7bc4016d01c0f5e9f
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2026-04-29 13:43:47 +0530

    fix:restructured the files

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -206,11 +206,10 @@ const Dashboard = () => {
                     </Link>
 
                     <Link href="https://twitter.com/itzznikhilsai" target="_blank">
-                        <Button className="flex items-center gap-2 px-4 py-2 cursor-pointer" variant="outline">
+                        <Button className="flex items-center gap-2 p-4 cursor-pointer" variant="outline">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
                             </svg>
-                            Twitter
                         </Button>
                     </Link>
 
